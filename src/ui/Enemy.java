078d28abcdbba3dd95cee19472daa5d6bbf71fe3
hanakakiko <1104349906@qq.com>
2021-01-28 14:26:33 +0800

    更改了生成子弹时的错误逻辑：用增强的for循环或迭代器遍历List时不能同时修改List

--- src/ui/Enemy.java
+++ src/ui/Enemy.java
@@ -2,26 +2,28 @@ package ui;
 
 import java.util.Random;
 
+//敌方战斗机类
 public class Enemy extends FlyObject{
-    int speed;
-    Enemy(){
+    int speed;//速度
+    public Enemy(){
         Random random=new Random();
         //出现的敌机图片随机
         int index=random.nextInt(15)+1;//[1,16)
         img=ImageUtil.getImage("/image/ep"+(index<10?"0":"")+index+".png");
-        speed=17-index;
-        assert img != null;
+        speed=17-index;//速度和图片索引有关
         w=img.getWidth();
         h=img.getHeight();
         //出现时的横坐标随机
         x=random.nextInt(512-w);
         y=-h;//这样让飞机感觉是从外面飞进来
     };
-    void move(){
 
-        y+=speed;
+    //移动
+    void move(){
+        y+=speed;//不同战斗机移动速度不同
     }
 
+    //判断战斗机是否被子弹击中（战斗机图片是否与子弹图片部分重合）
     public boolean isShot(Fire f) {
         return x<=f.x+f.w &&
                 x+w>=f.x &&
--- src/ui/ImageUtil.java
+++ src/ui/ImageUtil.java
@@ -4,7 +4,9 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 
+//对图片进行操作的工具类
 public class ImageUtil {
+    //根据路径获取图片
     public static BufferedImage getImage(String path){
         try {
             BufferedImage image;
